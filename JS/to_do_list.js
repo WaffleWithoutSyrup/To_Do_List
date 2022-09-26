6ee24b1fdc0909e1c79057f95385a11dd5b43cfe
Gabby <sad.duck.club3@gmail.com>
2022-09-26 17:19:49 -0500

    agregado js

--- JS/to_do_list.js
+++ JS/to_do_list.js
@@ -1,21 +1,53 @@
-const input= document.querySelector("input");
-const agregar =document.querySelector(".agregar");
-const ul = document.querySelector("ul");
-const vacio= document.querySelector(".sin-tareas");
+const input = document.querySelector('.input-btn input');
+const listTasks =document.querySelector('.list-tasks ul');
+const message =document.querySelector('.list-tasks');
+let tasks = [];
 
-agregar.addEventListener('click', (e)=>{
-    e.preventDefault();
-    const text = input.value;
-    const li = document.createElement('li');
-    p.textContent = text;
+function addTask() {
+    const task = input.value;
+    if (task === '') {
+        showError('No hay nada escrito');
+        return;
+    }
 
-    li.appendChild(p);
-    ul.appendChild(li);
-});
+    const taskObj = {
+        task,
+        id: Date.now()
+    }
+    tasks = [...task, taskObj]
 
-function add_delete_boton(){
-    const delete_btn = document.createElement('button');
-    delete_btn.textContent = "x";
-    delete_btn.className= "eliminar";
-    delete_btn.addEventListener('click')
+    createHTML();
+    input.value='';
+}
+
+function createHTML() {
+clearHTML();
+
+    if (tasks.length >0) {
+        tasks.forEach(task => {
+            const li = document.createElement('li');
+            li.innerHTML = `${task.task} <span task-id="${task.id}">X</span>`;
+
+            listTasks.appendChild(li);
+        });
+    }
+    
+}
+
+
+
+function showError(error) {
+    const messageError = document.createElement('p');
+    messageError.textContent = error;
+    messageError.classList.add('error');
+
+    message.appendChild(messageError);
+    //console.log(error);
+    setTimeout(()=> {
+        messageError.remove();
+    },2000);
+}
+
+function clearHTML() {
+    listTasks.innerHTML='';
 }
