60957d399180492dc0a8e3a1b2486c0baa1c20ff
Gabby <sad.duck.club3@gmail.com>
2022-09-26 20:14:00 -0500

    lista terminada

--- JS/to_do_list.js
+++ JS/to_do_list.js
@@ -3,6 +3,29 @@ const listTasks =document.querySelector('.list-tasks ul');
 const message =document.querySelector('.list-tasks');
 let tasks = [];
 
+eventListeners();       
+function eventListeners (){
+    document.addEventListener('DOMContentLoaded', ()=>{
+        tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+        createHTML();
+    });
+
+    listTasks.addEventListener('click', deleteTask);
+}
+
+function deleteTask(e){
+    if (e.target.tagName == 'SPAN') {
+        const deleteId = parseInt(e.target.getAttribute('task-id'));
+        tasks = tasks.filter(task => task.id !== deleteId);
+        createHTML();
+    }
+}
+
+function deleteAll(){
+    tasks = [];
+    createHTML();
+}
+
 function addTask() {
     const task = input.value;
     if (task === '') {
@@ -14,27 +37,30 @@ function addTask() {
         task,
         id: Date.now()
     }
-    tasks = [...task, taskObj]
+    tasks = [...tasks, taskObj]
 
     createHTML();
-    input.value='';
+    input.value = '';
 }
 
 function createHTML() {
-clearHTML();
+    clearHTML();
 
-    if (tasks.length >0) {
+    if (tasks.length > 0) {
         tasks.forEach(task => {
             const li = document.createElement('li');
-            li.innerHTML = `${task.task} <span task-id="${task.id}">X</span>`;
+            li.innerHTML = `${task.task} <span task-id="${task.id}" >X</span>`;
 
             listTasks.appendChild(li);
         });
     }
-    
+    sincronizarStorage();
 }
 
-
+function sincronizarStorage() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    
+}
 
 function showError(error) {
     const messageError = document.createElement('p');
