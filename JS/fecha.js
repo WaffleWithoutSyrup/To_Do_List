ccaaa86517da4ec600708e43717d74ae7f121884
Gabby <sad.duck.club3@gmail.com>
2022-09-30 12:57:07 -0500

    agregado fecha y cambios en estilos

---
+++ JS/fecha.js
@@ -0,0 +1,9 @@
+let fecha= new Date();
+let dia=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+
+let mes=['January','February','March','April','May','June','July','August','September','October','November','December']
+
+console.log(`${dia[fecha.getDay()]}, ${fecha.getDate()}, ${mes[fecha.getMonth()]}, ${fecha.getFullYear()}`);
+
+const fechaHtml= document.getElementById('#dateInList')
+console.log(fechaHtml);
